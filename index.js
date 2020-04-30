ea9f87d82d6f806e0081deb99f9a003c71eb9b9d
= <=>
2020-04-30 11:19:51 +0530

    recursion code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'https://apihub.document360.io/v1',
-    headers: {'Content-Type': 'application/json', 'api_token': '51xI6pLagkrhBUM9QQOE6oqexD6Kyh6g1CWx3RNoLZZ1hrIj54XffzYOOASn8qq0ohy1fo3uIqdg2VHNKkiUcjQO6+0OIExwkyGw3B46GNgNcwGsDISsZ9aKFijkh9fQd0YJJI+p/k9AFAcGB3G/kw=='}
+    baseURL: 'https://apihub.document360.info/v1',
+    headers: {'Content-Type': 'application/json', 'api_token': '51xI6pLagkrhBUM9QQOE6k7XFnbZYROx8krdClXBxJsnOZUTAjmPzn4oJ4Pushs1nGHgpKxYCN84bCDEtqkg+6O6oUzbfSOx4zyuiOGhnNFy6b6m+sIxxL3dfARR5megBF9mvqs5woW492B5QHe/TA=='}
   });
 
 
@@ -15,6 +15,32 @@ instance.get('/ProjectVersions')
 
       instance.get(`/ProjectVersions/${projectVersionId}/categories`)
       .then(function (res) {
-          console.log(res.data);
+        Recursion('category', res.data.data[1], 'Parent');
         });
     });
+
+
+function Recursion(nodetype, obj, parent) {
+
+  if(nodetype == 'article')
+  {
+    console.log(`item:${obj.title} parent:${parent}`);
+  }
+  
+  //Add Push to files accordingly
+  if(nodetype == 'category')
+  {
+    console.log(`item:${obj.name} parent:${parent}`);
+
+    var subCategories = obj.child_categories;
+    var articles = obj.articles;
+
+    subCategories.forEach(element => {
+      Recursion('category', element, obj.name);
+    });
+    
+    articles.forEach(element => {
+      Recursion('article', element, obj.name);
+    });
+  }
+} 
